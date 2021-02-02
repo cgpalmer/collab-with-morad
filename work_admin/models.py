06f4b9bdfd82b1060e09fecc4da7d42cea30eadf
Chris Palmer <cgpalmer91@gmail.com>
2021-02-02 21:37:54 +0000

    Adding html

--- work_admin/models.py
+++ work_admin/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+# # Create your models here.
+# class create_course(View):
+#     form_class = MyForm
+#     initial = {'key': 'value'}
+#     template_name = 'form_template.html'
+
+#     def get(request):
+#         form = self.form_class(initial=self.initial)
+#         return render(request, self.template_name, {'form': form})
+
+#     def post(request):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             # <process form cleaned data>
+#             return HttpResponseRedirect('/success/')
+
+#         return render(request, self.template_name, {'form': form})
